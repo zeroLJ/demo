0bcb27d57122360345d47e4511611a88b97e909d
zeroLJ <940034240@qq.com>
2017-02-04 23:13:21 +0800

    3

--- SendPostTest/app/src/main/java/com/example/zero/sendposttest/MainActivity.java
+++ SendPostTest/app/src/main/java/com/example/zero/sendposttest/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv;
     ImageView imageview;
     String s;
-    Handler handler1 = new Handler();
+    Handler handler3 = new Handler();
     String action = "http://192.168.0.105/Mytest/post.php";
     String url = "http://192.168.0.105/Mytest/post.php";//发送请求的地址
     OkHttpClient client;
